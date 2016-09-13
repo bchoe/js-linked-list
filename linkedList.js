f8e6e585f7ce7bc9955bb62c12b9caf7d4d98d7c
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-12 18:12:27 -1000

    14 tests passing

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,37 @@
  */
 function linkedListGenerator(){
 
-}
+  var head = null;
+  var tail = null;
+  var newAdd = null;
+
+  return {
+    getHead: function(){
+      return head;
+    },
+    getTail: function(){
+      return tail;
+    },
+    add: function(value){
+      var newNode = {
+        value: value,
+        next: null
+      };
+      head = newNode;
+      tail = newNode;
+      return newNode;
+
+    },
+    remove: function(){
+
+    },
+    get: function(){
+
+    },
+    insert: function(){
+
+    }
+  };
+
+  }
+
