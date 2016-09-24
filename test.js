742cc13e04ea11bca2c46f832940c96399721e4e
Bryan Choe <bryanchoe@Bryans-MacBook-Air.local>
2016-09-23 19:22:34 -1000

    commit

---
+++ test.js
@@ -0,0 +1,32 @@
+function linkedListGenerator(){
+
+var newHead = null;
+var newTail = null;
+var newAdd = null;
+
+console.log();
+
+  return {
+    getHead: function(){
+      return newHead;
+    },
+    getTail: function(){
+      return newTail;
+    },
+    add: function(){
+
+    },
+    remove: function(){
+
+    },
+    get: function(){
+
+    },
+    insert: function(){
+
+    }
+  };
+
+
+  }
+  linkedListGenerator();
